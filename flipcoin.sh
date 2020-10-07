a7558d5811add9080086d8a5672662973ffd1a85
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-07 10:36:15 +0530

    added the count file

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -x
 
 echo "welcome to flipcoin problem"
+headcount=0;
+tailcount=0;
+while [[ $headcount -le 21 && $tailcount  -le 21 ]]
+do
+       coinvalue=$(( RANDOM%2 ));
+        if [ $coinvalue  -eq 1 ]
+        then
+             echo "head"
+             headcount=$(($headcount+1))
 
-coinvalue=$(( RANDOM%2 ));
-if [ $coinvalue  -eq 1 ]
-then
-    echo "head"
-else
-    echo "tail"
-fi
+        else
+              echo "tail"
+              tailcount=$(($tailcount+1))
+        fi
+done
+echo "head count" $(($headcount-1))
+echo "tailcount" $(($tailcount-1))
 
